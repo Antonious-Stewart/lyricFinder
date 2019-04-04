24c45663460ef58b7ad56e9d5e87fc3b22c5d7a9
Antonious-Stewart <antonious.stewart@gmail.com>
2019-04-04 15:06:32 -0400

    Updates

---
+++ precache-manifest.9af10cff5cd86536a3e9d9892248b0d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2678ad7d48e214e16218",
+    "url": "/lyricFinder/static/css/main.876c9a3d.chunk.css"
+  },
+  {
+    "revision": "2678ad7d48e214e16218",
+    "url": "/lyricFinder/static/js/main.d85a433c.chunk.js"
+  },
+  {
+    "revision": "0558d7a791616c73661c",
+    "url": "/lyricFinder/static/js/runtime~main.6a6d2297.js"
+  },
+  {
+    "revision": "f9c7336f6ec1d5af9031",
+    "url": "/lyricFinder/static/css/2.e42fb315.chunk.css"
+  },
+  {
+    "revision": "f9c7336f6ec1d5af9031",
+    "url": "/lyricFinder/static/js/2.3517c30a.chunk.js"
+  },
+  {
+    "revision": "8ee7fcb932540041e18ee4e034a14a89",
+    "url": "/lyricFinder/static/media/PotableEmbarrassedFrenchbulldog-small.8ee7fcb9.gif"
+  },
+  {
+    "revision": "85453a7524572c311f0579e623f639f8",
+    "url": "/lyricFinder/index.html"
+  }
+];
